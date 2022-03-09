6b7774ff014c188527163d1837076e69049d0652
nyambura00 <winnienyambura14@gmail.com>
2022-03-09 14:04:20 -0500

    Add tagline

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,9 @@ const Header = () => {
     return (
         <div>
             <h2 style={{display: "flex", justifyContent: "center", alignItems:"center" }}>Winnie Nyambura</h2>
-            <p style={{display: "flex", justifyContent: "center", alignItems:"center" }}></p>
+            <p style={{display: "flex", justifyContent: "center", alignItems:"center" }}>
+                ~In pursuit of living intentional, or not.
+            </p>
         </div>
     )
 }
